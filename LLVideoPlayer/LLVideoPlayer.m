b8cb6835d5bf2cb23c6183a84cd4396742d7b43f
whuxujiyuan <1147595470@qq.com>
2019-02-21 11:20:24 +0800

    fix

--- LLVideoPlayer/LLVideoPlayer.m
+++ LLVideoPlayer/LLVideoPlayer.m
@@ -632,15 +632,15 @@ typedef void (^VoidBlock) (void);
     // is current player's item
     ll_run_on_ui_thread(^{
         
-        [self seekToTimeInSecond:0 userAction:NO completionHandler:nil];
-
-        if ([self.delegate respondsToSelector:@selector(videoPlayerShouldReplayOnEnd:)]) {
-            if ([self.delegate videoPlayerShouldReplayOnEnd:self]) {
-                [self.avPlayer play];
-                return;
-            }
-        }
-        [self pauseContent];
+        [self pauseContentWithCompletionHandler:^{
+            [self seekToTimeInSecond:0 userAction:NO completionHandler:^(BOOL finished) {
+                if ([self.delegate respondsToSelector:@selector(videoPlayerShouldReplayOnEnd:)]) {
+                    if ([self.delegate videoPlayerShouldReplayOnEnd:self]) {
+                        [self.avPlayer play];
+                    }
+                }
+            }];
+        }];
     });
 }
 
